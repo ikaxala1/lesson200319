f3b3d654a0b134f361818a98dd1e78fef6bbf1a0
Iraki Khaladze <i.khaladze1@gmail.com>
2020-03-19 20:11:13 +0400

    third commit with 3 tests

--- src/Fibonacci.py
+++ src/Fibonacci.py
@@ -1,3 +1,3 @@
 memo = {}
 def fib(n):
-    return 1
+    return fib(n - 1) + fib(n - 2)
--- test/fibonacci_test.py
+++ test/fibonacci_test.py
@@ -13,7 +13,7 @@ class FibonacciTest(unittest.TestCase):
         self.assertEqual(1, fib(1))
     def testFib2(self):
         self.assertEqual(1, fib(2))
-    # def testFib3(self):
-    #     self.assertEqual(2, fib(3))
+    def testFib3(self):
+        self.assertEqual(2, fib(3))
     # def testWithBrute(self):
     #     self.assertEqual(bruteFib(40), fib(40))
